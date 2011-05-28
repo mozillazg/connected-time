3282ecae20d5ae5e00bdf8f4fde38e051d980bc8
mozillazg <opensource.mozillazg@gmail.com>
2011-05-28 16:26:42 +0800

    初步思路，伪代码

---
+++ countconnectedtime.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+isconnected = False
+starttime, endtime = None
+
+while True:
+    try:
+    
+    except:
+        if isconnected:
+            endtime
+            # 写入文件
+            write(starttime, endtime, endtime-starttime) # 结果舍弃秒数，秒入为分
+        isconnected = False
+    else:
+         if not isconneceted:
+            starttime
+            isconneceted = True
+        
+    time.sleep(60) # 每一分钟循环一次
